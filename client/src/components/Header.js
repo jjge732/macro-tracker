4568e54b446f3d118cbf3b7286360b808c0ca46e
John Eastwood <jjge732@gmail.com>
2019-09-06 15:25:16 -0500

    added basic style to page

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+        <span className="navBarSpan">My Macro Tracker</span>
+        <span className="navBarSpan">My Foods</span>
+        <span className="navBarSpan">My Meals</span>
+    </header>
+  );
+}
+
+export default Header;
