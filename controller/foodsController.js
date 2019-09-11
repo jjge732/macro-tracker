630e51518939144cef7c5b7689b23ef8a65d471b
John Eastwood <jjge732@gmail.com>
2019-09-11 12:07:30 -0500

    added ability to update a food's name or macros and the the ability to retrieve one particular food in the backend '

--- controller/foodsController.js
+++ controller/foodsController.js
@@ -8,6 +8,26 @@ module.exports = {
             .then(dbUser => {res.json(dbUser)})
             .catch(err => {res.status(422).json(err)});
     },
+    getFoodById: (req, res) => {
+        db.Food
+            .find({_id: req.body.foodId})
+            .then(dbFood => {res.json(dbFood)})
+            .catch(err => {res.status(422).json(err)});
+    },
+    updateFood: (req, res) => {
+        db.Food
+            .updateOne({_id: req.body.foodId},
+                {
+                    foodName: req.body.foodName,
+                    gramsCarbs: req.body.gramsCarbs,
+                    gramsFat: req.body.gramsFat,
+                    gramsProtein: req.body.gramsProtein
+                }, {
+                    omitUndefined: true
+                })
+            .then(dbFood => {res.json(dbFood)})
+            .catch(err => {res.status(422).json(err)});
+    },
     postFood: (req, res) => {
         db.Food
             .create({
--- routes/api/foods.js
+++ routes/api/foods.js
@@ -4,8 +4,12 @@ const foodsController = require('../../controller/foodsController');
 router.route("/")
     .post(foodsController.postFood);
 
-router.route("/:user")
+router.route("/user/:user")
     .get(foodsController.getUserWithFoods);
+
+router.route("/:foodId")
+    .get(foodsController.getFoodById)
+    .patch(foodsController.updateFood);
 
 router.route("/all")
     .get(foodsController.getAllFoods);
